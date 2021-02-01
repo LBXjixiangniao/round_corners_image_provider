10cdedb7295086deee44949fa3535e2f977b95be
sunfly <>
2021-02-01 16:22:52 +0800

    【修改】+ push：push信息修改

--- test/round_corners_image_provider_test.dart
+++ test/round_corners_image_provider_test.dart
@@ -4,10 +4,10 @@ import 'package:round_corners_image_provider/round_corners_image_provider.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+    // final calculator = Calculator();
+    // expect(calculator.addOne(2), 3);
+    // expect(calculator.addOne(-7), -6);
+    // expect(calculator.addOne(0), 1);
+    // expect(() => calculator.addOne(null), throwsNoSuchMethodError);
   });
 }
